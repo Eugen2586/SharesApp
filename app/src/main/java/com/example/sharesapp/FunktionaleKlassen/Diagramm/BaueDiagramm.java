6182ccf8cd1c5064d9c83c6bae6f8d34e54dc38b
ChrisK <Christian.Kuellmer@netdate-lab.de>
2020-02-17 16:59:25 +0100

    Diagramm Stuff

--- app/src/main/java/com/example/sharesapp/FunktionaleKlassen/Diagramm/BaueDiagramm.java
+++ app/src/main/java/com/example/sharesapp/FunktionaleKlassen/Diagramm/BaueDiagramm.java
@@ -1,22 +1,22 @@
 
 package com.example.sharesapp.FunktionaleKlassen.Diagramm;
 
+import com.anychart.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ArrayList;
 
 public class BaueDiagramm {
 
     BaueDiagramm(ArrayList werte){
-
-
-
+        buidDiagram();
     }
     public void getDiagramm(){
 
         
     }
 
+    public void buidDiagram(){
 
-
-
-
+    }
 }

810b58cba69033ad019b0f7d7e7a879567603487
ChrisK <Christian.Kuellmer@netdate-lab.de>
2020-02-17 14:27:38 +0100

    Symbols To companys

--- app/src/main/java/com/example/sharesapp/Model/Aktie.java
+++ app/src/main/java/com/example/sharesapp/Model/Aktie.java
@@ -1,13 +1,19 @@
 package com.example.sharesapp.Model;
 
-public class Aktie {
+import pl.zankowski.iextrading4j.api.stocks.Quote;
 
+public class Aktie{
+    //ToDo neue Variablen für die Aktie
+    Quote quote;
+    int menge;
 
-   /*public Aktie( Quote aktie ){
+    public Aktie( ){
 
     }
-    
-    */
+    public Aktie( Quote quote ){
+       this.quote = quote;
+    }
+    //ToDo Handelsstuff
 
 
 }

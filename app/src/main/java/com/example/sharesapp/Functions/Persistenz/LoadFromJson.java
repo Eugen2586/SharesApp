7f20ebe52bca8331e255bd42ffa8f2164e1725f1
ChrisK <Christian.Kuellmer@netdate-lab.de>
2020-02-17 10:49:41 +0100

    Add Persistenzklassen.

--- app/src/main/java/com/example/sharesapp/Functions/Persistenz/LoadFromJson.java
+++ app/src/main/java/com/example/sharesapp/Functions/Persistenz/LoadFromJson.java
@@ -1,4 +1,66 @@
 package com.example.sharesapp.Functions.Persistenz;
 
 public class LoadFromJson {
+
+    private VBox workspace;
+    Projektview p;
+
+    public void setFenster(Anzeige_Fenster fenster) {
+        this.fenster = fenster;
+    }
+
+    private Anzeige_Fenster fenster;
+
+    public void setH(HerkulesFenster h) {
+        this.h = h;
+    }
+
+    HerkulesFenster h;
+
+    public void readJson() throws IOException, ParseException {
+        FileReader fr = new FileReader("keep.dat");
+        BufferedReader br = new BufferedReader(fr);
+        String st = new String();
+        String line = new String();
+        while(line != null) {
+            line = br.readLine();
+            if(line != null) {
+                st = st + line;
+            }
+        }
+        JSONParser parser = new JSONParser();
+        JSONArray jsonar = (JSONArray)parser.parse(st);
+        for (Object t:jsonar) {
+            addProjekt(((JSONObject)t).get("projektname").toString());
+            for (Object ts:(JSONArray)((JSONObject)t).get("Array")){
+                Timestamp tsp;
+                tsp = new Timestamp();
+                tsp.setStemp(Long.parseLong(((JSONObject) ts).get("Time").toString()));
+                tsp.setMonth(((JSONObject) ts).get("Month").toString());
+                tsp.setMonth(((JSONObject) ts).get("Year").toString());
+                tsp.setDay(((JSONObject) ts).get("Day").toString());
+                p.addTimestamp(tsp);
+            } ;
+        }
+    }
+
+    private void addProjekt(String projektname){
+        final FXMLLoader load2 = new FXMLLoader(getClass().getResource("projektview.fxml"));
+        try {
+            final Parent root = load2.load();
+            Projektview g = load2.getController();
+            g.set(h);
+            p = g;
+            h.addObject(g);
+            g.setRoot(root);
+            g.setProjekatname(projektname);
+            workspace.getChildren().add(root);
+        } catch (IOException e) {
+            // TODO LogFile schreiben ergänzen.
+        }
+    }
+
+    public void setWorkspace(JsonData workspace) {
+        this.workspace = workspace;
+    }
 }
---
+++ app/src/main/java/com/example/sharesapp/Model/DataJson.java
@@ -0,0 +1,10 @@
+package com.example.sharesapp.Model;
+
+public class DataJson {
+    //ToDo Chris K.
+    //Implemtier hier die generische Persistenz um diese in der Schnittstellenklasse DataJson zu pflegen
+    
+
+
+
+}

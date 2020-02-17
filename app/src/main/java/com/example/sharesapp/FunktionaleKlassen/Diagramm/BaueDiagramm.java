068b0d86d19900154383b84f997d5a696be02ddc
ChrisK <Christian.Kuellmer@netdate-lab.de>
2020-02-17 16:51:54 +0100

    BugFixStuff

---
+++ app/src/main/java/com/example/sharesapp/FunktionaleKlassen/Diagramm/BaueDiagramm.java
@@ -0,0 +1,18 @@
+package com.example.sharesapp.FunktionaleKlassen.Diagramm;
+
+import java.util.ArrayList;
+
+public class BaueDiagramm {
+
+    BaueDiagramm(ArrayList werte){
+
+
+
+    }
+    get
+
+
+
+
+
+}

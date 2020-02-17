57788a806e0aeefd01f04cab753dc5011ffa7279
ChrisK <Christian.Kuellmer@netdate-lab.de>
2020-02-17 11:37:13 +0100

    Generischen Anteil von write Json für persistens. :)

--- app/src/main/java/com/example/sharesapp/Functions/Persistenz/SaveInJson.java
+++ app/src/main/java/com/example/sharesapp/Functions/Persistenz/SaveInJson.java
@@ -1,4 +1,43 @@
 package com.example.sharesapp.Functions.Persistenz;
 
+import com.example.sharesapp.Model.DataJson;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class SaveInJson {
+    ArrayList objects = new ArrayList();
+    JSONArray b = new JSONArray();
+    public void printGson() throws IOException {
+        JSONArray jar = new JSONArray();
+        for (Object p:objects) {
+            //ToDo Implementiert in diesem Style
+            //jar.add(createJSON((Projektview)p));
+        }
+        FileWriter frw = new FileWriter("keep.dat");
+        frw.write(String.valueOf(jar));
+        frw.flush();
+        frw.close();
+    }
+    private JSONObject createJSON(DataJson p){
+        JSONObject a = new JSONObject();
+        /*ToDo schreibt eure JSON in diesme Stil!
+        a.put("projektname",p.getProjekatname());
+        JSONArray c = new JSONArray();
+        for (Object t:p.getTimestamps()) {
+            JSONObject d = new JSONObject();
+            d.put("Day",((Timestamp) t).getDay());
+            d.put("Time", ((Timestamp) t).getStemp());
+            d.put("Month",((Timestamp) t).getDay());
+            d.put("Year",((Timestamp) t).getDay());
+            c.add(d);
+        }
+        a.put("Array", c);
+         */
+        return a;
+    }
 }

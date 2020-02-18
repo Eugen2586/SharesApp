8597e9e3b5bb102dd2c80e35c6c131938d40c346
atsarenko <riannoldor@gmail.com>
2020-02-18 10:29:38 +0100

    write fragments to uebersicht- and statistikfragment

--- app/src/main/java/com/example/sharesapp/ui/depot/statistik/StatistikFragment.java
+++ app/src/main/java/com/example/sharesapp/ui/depot/statistik/StatistikFragment.java
@@ -1,6 +1,20 @@
 package com.example.sharesapp.ui.depot.statistik;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sharesapp.R;
+
 import androidx.fragment.app.Fragment;
 
 public class StatistikFragment  extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_depot_statistik, container, false);
+    }
 }

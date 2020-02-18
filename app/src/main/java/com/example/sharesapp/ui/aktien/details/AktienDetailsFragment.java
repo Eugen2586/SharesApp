0a2c748af5c140fa81b01999ba73a15a7c44d927
atsarenko <riannoldor@gmail.com>
2020-02-18 13:22:03 +0100

    bugfix

--- app/src/main/java/com/example/sharesapp/ui/aktien/details/AktienDetailsFragment.java
+++ app/src/main/java/com/example/sharesapp/ui/aktien/details/AktienDetailsFragment.java
@@ -15,6 +15,6 @@ public class AktienDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_depot_uebersicht, container, false);
+        return inflater.inflate(R.layout.fragment_aktien_details, container, false);
     }
 }

ec60a0de42a552799fe47ed369145e7be6727637
gvo <gianluca.voss@octavia.de>
2020-02-18 12:50:40 +0100

    adds stock data url

--- app/src/main/java/com/example/sharesapp/REST/RequestsBuilder.java
+++ app/src/main/java/com/example/sharesapp/REST/RequestsBuilder.java
@@ -2,16 +2,20 @@ package com.example.sharesapp.REST;
 
 public class RequestsBuilder {
 
-    public static String getTimeSeriesURL(String key) {
-        return "time-series/REPORTED_FINANCIALS/" + key;
+    public static String getTimeSeriesURL(String symbol) {
+        return "time-series/REPORTED_FINANCIALS/" + symbol;
     }
 
     public static String getAllSymbolsURL() {
-        return "/ref-data/symbols";
+        return "ref-data/symbols";
     }
 
     public String getSearchURL(String fragment) {
-        return "/search/{" + fragment + "}";
+        return "search/{" + fragment + "}";
+    }
+
+    public String getQuoteData(String symbol) {
+        return "data-points/" + symbol;
     }
 
 }

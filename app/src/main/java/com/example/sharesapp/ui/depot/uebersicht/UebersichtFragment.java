21f5a65d364b49a5dbcee8ab94989ec8a9046741
levinSchu <levinschubotz@gmx.net>
2020-02-18 13:36:05 +0100

    started own stock overview

--- app/src/main/java/com/example/sharesapp/ui/depot/uebersicht/UebersichtFragment.java
+++ app/src/main/java/com/example/sharesapp/ui/depot/uebersicht/UebersichtFragment.java
@@ -8,13 +8,35 @@ import android.view.ViewGroup;
 import com.example.sharesapp.R;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class UebersichtFragment extends Fragment {
+
+    StockRecyclerViewAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_depot_uebersicht, container, false);
+
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = root.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        adapter = new StockRecyclerViewAdapter(this.getContext(), animalNames);
+//        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
         return inflater.inflate(R.layout.fragment_depot_uebersicht, container, false);
     }
 

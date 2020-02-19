72ea973984b3ab54a13b406ab9a36bc67f03b494
ChrisK <Christian.Kuellmer@netdate-lab.de>
2020-02-19 13:32:03 +0100

    Währungsumrechnung

---
+++ app/src/main/java/com/example/sharesapp/FunktionaleKlassen/Waehrungen/Umrechnung.java
@@ -0,0 +1,7 @@
+package com.example.sharesapp.FunktionaleKlassen.Waehrungen;
+
+public class Umrechnung {
+    public float atob(float cash, float wechselkurs){
+        return cash * wechselkurs;
+    }
+}

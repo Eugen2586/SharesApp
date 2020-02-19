e9454d272332374dbacd85f88f645e84290e3d9e
levinSchu <levinschubotz@gmx.net>
2020-02-19 15:30:10 +0100

    implemented chartBuilder

--- app/src/main/java/com/example/sharesapp/ui/depot/statistik/StatistikFragment.java
+++ app/src/main/java/com/example/sharesapp/ui/depot/statistik/StatistikFragment.java
@@ -7,21 +7,25 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.sharesapp.FunktionaleKlassen.Diagramm.ChartDataBuilder;
 import com.example.sharesapp.R;
 import com.google.android.material.tabs.TabLayout;
+
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class StatistikFragment extends Fragment {
 
     TabLayout tabs;
+    View root;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_depot_statistik, container, false);
+        root = inflater.inflate(R.layout.fragment_depot_statistik, container, false);
 
-//        LineChartView lineChartView = root.findViewById(R.id.line_chart_view);
-//        ChartDataBuilder ChartDataBuilder = new ChartDataBuilder();
-//        lineChartView.setLineChartData(ChartDataBuilder.getData());
+        LineChartView lineChartView = root.findViewById(R.id.line_chart_view);
+        ChartDataBuilder ChartDataBuilder = new ChartDataBuilder(0);
+        lineChartView.setLineChartData(ChartDataBuilder.getData());
 
         tabs = root.findViewById(R.id.statistik_tab_layout);
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -43,15 +47,8 @@ public class StatistikFragment extends Fragment {
     }
 
     private void changeFragment(int position) {
-        switch (position) {
-            case 0:
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            default:
-                System.out.println("Falsche Position");
-        }
+        LineChartView lineChartView = root.findViewById(R.id.line_chart_view);
+        ChartDataBuilder ChartDataBuilder = new ChartDataBuilder(position);
+        lineChartView.setLineChartData(ChartDataBuilder.getData());
     }
 }

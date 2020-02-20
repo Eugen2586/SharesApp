f08f703da629e389d08b08ca0c2a9a6e3504dbcc
atsarenko <riannoldor@gmail.com>
2020-02-20 12:46:19 +0100

    made function onClick in AktienDetailsFragment to show the KaufDialog

--- app/src/main/java/com/example/sharesapp/ui/aktien/details/AktienDetailsFragment.java
+++ app/src/main/java/com/example/sharesapp/ui/aktien/details/AktienDetailsFragment.java
@@ -1,20 +1,60 @@
 package com.example.sharesapp.ui.aktien.details;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.sharesapp.R;
 
 public class AktienDetailsFragment extends Fragment {
 
+    View root;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        root = inflater.inflate(R.layout.fragment_aktien_details, container, false);
         // Inflate the layout for this fragment
+        final View buyDialogView = inflater.inflate(R.layout.buy_dialog, null);
+        Button buy_button = root.findViewById(R.id.kaufen_button);
+
+        buy_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = AktienDetailsFragment.this.getContext();
+                if (context != null) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setCancelable(true);
+                    builder.setView(buyDialogView);
+                    builder.setPositiveButton("Ja",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //todo kaufen
+                                }
+                            });
+                    builder.setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
         return inflater.inflate(R.layout.fragment_aktien_details, container, false);
     }
 }

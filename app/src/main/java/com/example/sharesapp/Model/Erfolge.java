20ae2b15c598b7ec4561fc7508ee33bd272abe40
atsarenko <riannoldor@gmail.com>
2020-03-26 17:06:36 +0100

    Update Erfolge.java

--- app/src/main/java/com/example/sharesapp/Model/Erfolge.java
+++ app/src/main/java/com/example/sharesapp/Model/Erfolge.java
@@ -1,4 +1,9 @@
 package com.example.sharesapp.Model;
 
 public class Erfolge {
+
+    private boolean[] kaufen = new boolean[4];
+    private boolean[] verkaufen = new boolean[4];
+    private boolean[] reset = new boolean[4];
+
 }

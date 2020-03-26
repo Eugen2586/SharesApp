ba13f2b3f5fb7b616f90c35953e21c407283517d
levinSchu <levinschubotz@gmx.net>
2020-03-26 11:14:28 +0100

    prepared statistic for anychart (continue if stockprice bug fixed)

--- app/src/main/java/com/example/sharesapp/ui/depot/statistik/StatistikFragment.java
+++ app/src/main/java/com/example/sharesapp/ui/depot/statistik/StatistikFragment.java
@@ -6,49 +6,100 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Stock;
+import com.anychart.core.stock.Plot;
+import com.anychart.core.stock.series.Hilo;
+import com.anychart.core.stock.series.Line;
+import com.anychart.data.Table;
+import com.anychart.data.TableMapping;
+import com.example.sharesapp.FunktionaleKlassen.Diagramm.AnyChartDataBuilder;
 import com.example.sharesapp.FunktionaleKlassen.Diagramm.ChartDataBuilder;
+import com.example.sharesapp.Model.Constants;
+import com.example.sharesapp.Model.FromServerClasses.Aktie;
+import com.example.sharesapp.Model.FromServerClasses.Trade;
+import com.example.sharesapp.Model.Model;
 import com.example.sharesapp.R;
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 import lecho.lib.hellocharts.view.LineChartView;
 
 public class StatistikFragment extends Fragment {
 
-    TabLayout tabs;
-    View root;
+    private View root;
+    private Model model = new Model();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_depot_statistik, container, false);
 
-        LineChartView lineChartView = root.findViewById(R.id.line_chart_view);
-        ChartDataBuilder ChartDataBuilder = new ChartDataBuilder(0);
-        lineChartView.setLineChartData(ChartDataBuilder.getData());
-
-        tabs = root.findViewById(R.id.statistik_tab_layout);
-        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        final Observer<ArrayList<Trade>> tradesObserver = new Observer<ArrayList<Trade>>() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                changeFragment(tab.getPosition());
+            public void onChanged(ArrayList<Trade> tradeList) {
+                showLineChart(tradeList);
             }
+        };
+        model.getData().getTradesMutable().observe(getViewLifecycleOwner(), tradesObserver);
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
+        showLineChart(model.getData().getTrades());
 
         return root;
     }
 
-    private void changeFragment(int position) {
-        LineChartView lineChartView = root.findViewById(R.id.line_chart_view);
-        ChartDataBuilder ChartDataBuilder = new ChartDataBuilder(position);
-        lineChartView.setLineChartData(ChartDataBuilder.getData());
+    private void showLineChart(ArrayList<Trade> tradeList) {
+        // https://github.com/AnyChart/AnyChart-Android/blob/master/sample/src/main/java/com/anychart/sample/charts/LineChartActivity.java
+        if (tradeList != null) {
+            // Build the stockdatachart
+            Cartesian cartesian = AnyChart.line();
+            cartesian.animation(true);
+
+            // create data
+            ArrayList<DataEntry> dataList = generateDataFromTradeList(tradeList);
+
+            // create a line series and set the data
+            cartesian.line(dataList);
+
+            // set the container id
+            cartesian.container("container");
+
+            // change visibility of components
+            AnyChartView anyChartView = root.findViewById(R.id.any_chart_view);
+            anyChartView.setChart(cartesian);
+//            cartesian.draw(false);
+            anyChartView.setVisibility(View.VISIBLE);
+            root.findViewById(R.id.no_chart_view).setVisibility(View.GONE);
+        } else {
+            root.findViewById(R.id.no_chart_view).setVisibility(View.VISIBLE);
+            root.findViewById(R.id.any_chart_view).setVisibility(View.GONE);
+        }
+    }
+
+    private ArrayList<DataEntry> generateDataFromTradeList(ArrayList<Trade> tradeList) {
+        ArrayList<DataEntry> dataList = new ArrayList<>();
+        float currentMoney = Constants.MONEY;
+        for (Trade trade : tradeList) {
+            if(trade.isKauf()) {
+                currentMoney -= trade.getPreis();
+            } else {
+                currentMoney += trade.getPreis();
+            }
+            dataList.add(createDataEntry(trade.getDate(), currentMoney));
+        }
+
+        return dataList;
+    }
+
+    private DataEntry createDataEntry(String x, float y) {
+        DataEntry dataEntry = new DataEntry();
+        dataEntry.setValue(x, y);
+        return dataEntry;
     }
 }
